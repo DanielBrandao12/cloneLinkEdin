9e4ea2613e9afa15d3af071e17605cdfbc302e21
DanielBrandao12 <drb12da@gmail.com>
2023-03-09 11:20:08 -0300

    atualização header e scene inicio

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,7 +13,7 @@ function Header() {
                 <div class="div-logo-search">
                     <a>
                         <div>
-                            <img src="iconelinkedin.png" />
+                            <img alt="perfil foto" src="iconelinkedin.png" />
                         </div>
                     </a>
                     <div>
@@ -25,19 +25,9 @@ function Header() {
                     <ul>
 
                         <li class="links" >
-                            <Link to="/"  class="ancora">
-                                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                                    width="50.000000pt" height="50.000000pt" viewBox="0 0 50.000000 50.000000"
-                                    preserveAspectRatio="xMidYMid meet">
-
-                                    <g transform="translate(0.000000,50.000000) scale(0.100000,-0.100000)"
-                                        fill="#000000" stroke="none">
-                                        <path d="M130 399 c-109 -85 -144 -120 -106 -106 14 6 16 -8 16 -128 l0 -135
-75 0 75 0 0 90 0 90 60 0 60 0 0 -90 0 -90 75 0 75 0 0 135 c0 120 2 134 16
-128 37 -14 3 20 -106 106 -63 50 -117 91 -120 90 -3 0 -57 -40 -120 -90z"/>
-                                        <path d="M370 425 c13 -14 27 -25 32 -25 4 0 8 11 8 25 0 21 -5 25 -32 25
-l-31 0 23 -25z"/>
-                                    </g>
+                            <Link to="/" class="ancora">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                    <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
                                 </svg>
 
                                 Início
@@ -46,78 +36,32 @@ l-31 0 23 -25z"/>
                         </li>
                         <li class="links">
                             <Link to="/MinhaRede" class="ancora">
-                                <svg class="minhaRede" version="1.0" xmlns="http://www.w3.org/2000/svg"
-                                    width="64.000000pt" height="64.000000pt" viewBox="0 0 64.000000 64.000000"
-                                    preserveAspectRatio="xMidYMid meet">
-
-                                    <g transform="translate(0.000000,64.000000) scale(0.100000,-0.100000)"
-                                        fill="#000000" stroke="none">
-                                        <path d="M384 500 c-47 -19 -67 -77 -42 -123 26 -50 77 -61 121 -27 49 39 42
-116 -14 145 -32 17 -36 17 -65 5z"/>
-                                        <path d="M173 456 c-22 -19 -28 -32 -28 -65 0 -48 24 -77 72 -87 27 -5 37 -1
-63 24 40 41 41 83 1 123 -36 35 -70 37 -108 5z"/>
-                                        <path d="M357 302 c-16 -7 -15 -11 12 -37 17 -15 34 -43 37 -62 6 -33 6 -33
-64 -33 89 0 117 41 63 92 -46 44 -122 61 -176 40z"/>
-                                        <path d="M145 265 c-61 -31 -81 -77 -47 -108 15 -14 40 -17 126 -17 59 0 116
-5 127 10 41 23 16 86 -48 119 -31 16 -123 14 -158 -4z"/>
-                                    </g>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                    <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
                                 </svg>
                                 Minha Rede
                             </Link>
                         </li>
                         <li class="links">
                             <Link class="ancora">
-                                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                                    width="64.000000pt" height="64.000000pt" viewBox="0 0 64.000000 64.000000"
-                                    preserveAspectRatio="xMidYMid meet">
-
-                                    <g transform="translate(0.000000,64.000000) scale(0.100000,-0.100000)"
-                                        fill="#000000" stroke="none">
-                                        <path d="M226 484 c-3 -9 -6 -121 -6 -250 l0 -234 100 0 100 0 0 234 c0 129
--3 241 -6 250 -9 23 -179 23 -188 0z m164 -44 l0 -30 -70 0 -70 0 0 30 0 30
-70 0 70 0 0 -30z"/>
-                                        <path d="M83 402 c-6 -4 -17 -20 -22 -35 -14 -37 -14 -275 0 -315 11 -33 49
--52 100 -52 l29 0 0 205 0 205 -47 0 c-27 0 -53 -4 -60 -8z"/>
-                                        <path d="M450 205 l0 -205 29 0 c51 0 89 19 100 52 14 40 14 278 0 315 -14 37
--25 43 -81 43 l-48 0 0 -205z"/>
-                                    </g>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                    <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
                                 </svg>
                                 Vagas
                             </Link>
                         </li>
                         <li class="links">
                             <Link class="ancora">
-                                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                                    width="96.000000pt" height="96.000000pt" viewBox="0 0 96.000000 96.000000"
-                                    preserveAspectRatio="xMidYMid meet">
-
-                                    <g transform="translate(0.000000,96.000000) scale(0.100000,-0.100000)"
-                                        fill="#000000" stroke="none">
-                                        <path d="M375 866 c-112 -28 -215 -109 -262 -205 -23 -47 -27 -69 -27 -141 0
--72 4 -94 27 -141 61 -125 214 -219 359 -219 l47 0 3 -61 3 -61 64 43 c81 54
-186 161 228 230 44 73 67 168 59 242 -23 221 -264 372 -501 313z m-15 -346 l0
--40 -40 0 -40 0 0 40 0 40 40 0 40 0 0 -40z m160 0 l0 -40 -40 0 -40 0 0 40 0
-40 40 0 40 0 0 -40z m160 0 l0 -40 -40 0 -40 0 0 40 0 40 40 0 40 0 0 -40z"/>
-                                    </g>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                    <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
                                 </svg>
                                 Mensagens
                             </Link>
                         </li>
                         <li class="links">
                             <Link class="ancora">
-                                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                                    width="60.000000pt" height="60.000000pt" viewBox="0 0 60.000000 60.000000"
-                                    preserveAspectRatio="xMidYMid meet">
-
-                                    <g transform="translate(0.000000,60.000000) scale(0.100000,-0.100000)"
-                                        fill="#000000" stroke="none">
-                                        <path d="M272 528 c-6 -6 -12 -18 -12 -25 0 -7 -6 -13 -14 -13 -23 0 -78 -51
--92 -87 -8 -18 -14 -59 -14 -92 0 -33 -4 -71 -10 -85 l-10 -26 181 0 c139 0
-178 3 173 12 -4 7 -11 51 -14 99 -5 75 -10 93 -35 125 -15 20 -40 42 -55 47
--15 6 -31 20 -36 31 -11 24 -44 32 -62 14z"/>
-                                        <path d="M85 150 c-13 -22 9 -30 79 -30 l74 0 6 -30 c12 -63 100 -63 112 -1 6
-30 7 30 83 33 60 2 76 6 76 18 0 13 -31 15 -212 18 -136 1 -214 -1 -218 -8z"/>
-                                    </g>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                    <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
                                 </svg>
                                 Notificações
                             </Link>
@@ -128,8 +72,18 @@ l-31 0 23 -25z"/>
                                 Eu
                             </Link>
                         </li>
-                        <li>Soluções</li>
-                        <li>Reative Premium: 50% de desconto</li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
+                            </svg>
+                            <div class="solucao">
+                                Soluções
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" class="mercado-match" width="16" height="16" focusable="false">
+                                    <path d="M8 11L3 6h10z" fill-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                        </li>
+                        <li class="promo"><a>Reative o Premium</a></li>
                     </ul>
                 </nav>
             </div>
--- src/scenes/inicio/index.js
+++ src/scenes/inicio/index.js
@@ -1,15 +1,62 @@
 import Header from "../../components/Header"
-import AsideLeft from "../../components/AsideLeft"
-import DivPost from "../../components/DivPost"
 
+import "./inicio.css"
 
 function Inicio() {
   return (
     <>
 
       <Header />
-      <AsideLeft />
-      <DivPost/>
+        <main>
+
+          <aside class="asideLeft">
+            <div>
+              <img alt="perfil foto" src="eu.jpg" />
+
+            </div>
+          </aside>
+
+          <section class="container-post">
+            <div class="container-publi">
+              <img alt="perfil foto" src="eu.jpg" />
+              <button>
+                <span>Começar publicação</span>
+              </button>
+            </div>
+            <div class="type-posts">
+
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                  <path fill="#378fe9" d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm1 13a1 1 0 01-.29.71L16 14l-2 2-6-6-4 4V7a1 1 0 011-1h14a1 1 0 011 1zm-2-7a2 2 0 11-2-2 2 2 0 012 2z"></path>
+                </svg>
+                Foto
+              </div>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                  <path fill="#5f9b41" d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm-9 12V8l6 4z"></path>
+                </svg>
+                Vídeo
+
+              </div>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                  <path fill="#a872e8" d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
+                </svg>
+                Vaga
+              </div>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                  <path fill="#e16745" d="M21 3v2H3V3zm-6 6h6V7h-6zm0 4h6v-2h-6zm0 4h6v-2h-6zM3 21h18v-2H3zM13 7H3v10h10z"></path>
+                </svg>
+                Escrever artigo
+              </div>
+            </div>
+          </section>
+
+          <aside class="asideRight">
+
+          </aside>
+        </main>
 
     </>
   );
